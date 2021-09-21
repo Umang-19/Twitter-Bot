fdc53c8fdad0238a4053d992c8b3bbc1f047c08c
Umang <umangaggarwal537@gmail.com>
2021-09-21 11:28:17 +0530

    adding dotenv

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var Twit = require("twit");
-var config = require("./config");
-var T = new Twit(config);
+// var config = require("./config");
+require('dotenv').config();
+// var T = new Twit(config);
+
+var T = new Twit({
+    consumer_key:         process.env.CONSUMER_KEY,
+    consumer_secret:      process.env.CONSUMER_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+  })
 
 console.log("Welcome to Umang's Bot");
 
 function retweet() 
 {
-    let params = { q:'#100daysofcodewithGFG', result_type:'recent', count:100 }
+    let params = { q:'#100daysofcodewithGFG', result_type:'recent', count:20 }
 
     T.get('search/tweets', params, gotData);
 
@@ -16,7 +24,7 @@ function retweet()
             console.log("Some Error occurred!");
         else 
         {
-            console.log(data);
+            // console.log(data);
             var tweets = data.statuses;
             console.log("GOT THESE TWEETS");
             for (var i = 0; i < tweets.length; i++) 
@@ -29,9 +37,9 @@ function retweet()
                 T.post('statuses/retweet/:id', {id: retweetId}, (err, response)=>
                 {
                     if(response)
-                        console.log('Post retweeted successfully!!!');
-                    if(err)
-                    console.log("Some error occurred!");
+                        console.log('Post retweeted successfully!!!');    
+                    else
+                        console.log("Some error occurred!");    
                 })
             }
         }
