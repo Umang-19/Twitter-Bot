54f5c7a66f5881f0026cef0ec83e058f26c661a0
Umang <umangaggarwal537@gmail.com>
2021-09-21 13:48:53 +0530

    changed count

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 // var T = new Twit(config);
 
 var T = new Twit({
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+    consumer_key:         process.env.consumer_key,
+    consumer_secret:      process.env.consumer_secret,
+    access_token:         process.env.access_token,
+    access_token_secret:  process.env.access_token_secret
   })
 
 console.log("Welcome to Umang's Bot");
